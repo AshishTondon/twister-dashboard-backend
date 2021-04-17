6352c70c57f2881428e4cde7f1092558262a7feb
Ashish Tondon <tondon.ashish@hotmail.com>
2021-04-17 14:15:33 +0530

    fixed db connection

--- app/controllers/database/employee.ts
+++ app/controllers/database/employee.ts
@@ -18,16 +18,18 @@ interface IN_pagination{
 const getConnection = async() =>{
     
     if(connection === null){
-        connection = new Pool({
-                host: 'ec2-54-164-22-242.compute-1.amazonaws.com',
-                user: 'ilnfyvnzpllmit',
-                max: 20,
-                idleTimeoutMillis: 30000,
-                connectionTimeoutMillis: 2000,
-                dbname: 'denrirfmj8mrv2',
-                password: '41e32323500a322ddf9017c7efa14f57353bdf6dd2b81ce72189af1f8d1aa9f4',
-                port: 5432
-            });
+        connection = new Pool();
+
+        // connection = new Pool({
+        //         host: 'ec2-54-164-22-242.compute-1.amazonaws.com',
+        //         user: 'ilnfyvnzpllmit',
+        //         max: 20,
+        //         idleTimeoutMillis: 30000,
+        //         connectionTimeoutMillis: 2000,
+        //         dbname: 'denrirfmj8mrv2',
+        //         password: '41e32323500a322ddf9017c7efa14f57353bdf6dd2b81ce72189af1f8d1aa9f4',
+        //         port: 5432
+        //     });
         
         /*connection = await mysql.createConnection({
             host: 'localhost',
